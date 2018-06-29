395d7eceedde0bb4b37bb37b1b8e9db309938d43
DerarAlmstoo <38909797+DerarAlmstoo@users.noreply.github.com>
2018-06-29 09:14:12 +0200

    week4

---
+++ homework4/js/js1.js
@@ -0,0 +1,133 @@
+console.log('Hello World!');
+console.log('Halo, dunia!');
+console.log('Ciao, mondo! ');
+console.log('Hola, mundo!');
+console.log('Im awesome');
+
+//////////////////////////////////////////////////////
+
+var x ;
+console.log('the value of my variable x will be: 1');
+x=1;
+console.log('the value of my variable x will be: 2');
+x=2;
+
+var y ;
+console.log('the value of my variable y will be: 3');
+y=3;
+console.log('the value of my variable y will be: 4');
+y=4;
+
+//////////////////////////////////////////////////////
+
+Math.round(7.25);
+var z = 7.25;
+console.log(z);
+var a= Math.round(z);
+console.log(a);
+var c;
+if(a>z){c=a;}
+else{c=z;}
+console.log(c);
+
+////////////////////////////////////////
+
+var myArray = [];
+console.log('the value of my variable y will be: 1 2 3 4');
+console.log(myArray);
+myArray[0] = 1;
+myArray[1] = 2;
+myArray[2] = 3;
+myArray[3] = 4;
+console.log(myArray);
+
+var animals = ['dog','cat','donky'];
+console.log(animals);
+animals.push('baby pig');
+console.log(animals);
+
+///////////////////////////////////////////////
+
+let myString = "this is a test";
+console.log('myString');
+var n = myString.length;
+console.log(n);
+
+////////////////////////////////////////////
+
+var a;
+var b;
+if(typeof(a)==typeof(b)){
+ console.log(typeof(a));
+ console.log('SAME TYPE');
+}
+
+var u =1;
+var i = "hi";
+var o = true ;
+var p = [];
+console.log(u);
+console.log(i);
+console.log(o);
+console.log(p);
+
+let foo = 3;
+console.log('The value of my variable foo is: ' + foo);
+console.log('The value of my variable foo is an integer');
+console.log(typeof(foo));
+
+if(typeof(foo)==typeof(u)){
+    console.log(typeof(foo));
+    console.log('SAME TYPE');
+   }
+else{console.log('not the SAME TYPE')}
+
+   if(typeof(foo)==typeof(i)){
+    console.log(typeof(foo));
+    console.log('SAME TYPE');
+   }
+else{console.log('not the SAME TYPE')}
+
+   if(typeof(foo)==typeof(o)){
+    console.log(typeof(foo));
+    console.log('SAME TYPE');
+   }
+   else{console.log('not the SAME TYPE')}
+
+   if(typeof(foo)==typeof(p)){
+    console.log(typeof(foo));
+    console.log('SAME TYPE');
+   }
+   else{console.log('not the SAME TYPE')}
+
+////////////////
+
+x=7;
+x = x % 3;
+console.log(x);
+
+x=9;
+x = x % 4;
+console.log(x);
+
+x=11;
+x = x % 5;
+console.log(x);
+
+x=13;
+x = x % 4;
+console.log(x);
+
+//////////////////////////////////
+
+var Arr =[1,'hi',true];
+
+// Can you compare infinities? (Not in Eyad's world) - does 6/0 === 10/0? How can you test this? ( i dont understand this question)
+
+
+
+
+
+
+
+
